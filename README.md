3d0305875ec556ef99a140765ddc67ea8fe29286
Athul P <athul929@gmail.com>
2021-05-17 00:29:39 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation
+1. Clone.
+2. `npm install`
+3. `npm run migrate`
+4. `npm run build`
+5. `npm run start`
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import NITC from '../nitc'
 function Footer() {
   return (
     <footer className="container mx-auto fixed bottom-0 right-0 left-0 flex justify-center items-center h-24">
-      <a href="https://library.nitc.ac.in">
+      <a href="http://library.nitc.ac.in">
         <NITC />
       </a>
     </footer>
